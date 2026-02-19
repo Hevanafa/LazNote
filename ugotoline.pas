887e8117064cc859a3f57b4607cbc595042dcbcf
Hevanafa <dhevansafa4@gmail.com>
2026-02-19 15:39:14 +0700

    added more convenience to close go to line

--- ugotoline.pas
+++ ugotoline.pas
@@ -7,7 +7,8 @@ interface
 
 uses
   Classes, SysUtils, Forms,
-  Controls, Graphics, Dialogs, StdCtrls, Spin;
+  Controls, Graphics, Dialogs,
+  StdCtrls, Spin, LCLType;
 
 type
 
@@ -18,7 +19,11 @@ type
     CancelButton: TButton;
     LineNumberLabel: TLabel;
     LineNumberEdit: TSpinEdit;
+    procedure AcceptButtonKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure CancelButtonClick(Sender: TObject);
+    procedure CancelButtonKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure LineNumberEditKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
   private
 
   public
@@ -35,6 +40,26 @@ procedure TGoToLine.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
 begin
   CanClose := false;
   hide
+end;
+
+procedure TGoToLine.AcceptButtonKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if Key = VK_ESCAPE then close;
+end;
+
+procedure TGoToLine.CancelButtonClick(Sender: TObject);
+begin
+  close
+end;
+
+procedure TGoToLine.CancelButtonKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if Key = VK_ESCAPE then close;
+end;
+
+procedure TGoToLine.LineNumberEditKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if Key = VK_ESCAPE then close;
 end;
 
 procedure TGoToLine.SetMaximum(n: longint);

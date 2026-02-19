1974a552af4f12cec2b889540ff8581022e78ad8
Hevanafa <dhevansafa4@gmail.com>
2026-02-19 10:45:04 +0700

    First upload

---
+++ unit1.pas
@@ -0,0 +1,34 @@
+unit Unit1;
+
+{$Mode ObjFPC}
+{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms,
+  Controls, Graphics, Dialogs, Menus, StdCtrls, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    Memo1: TMemo;
+    StatusBar1: TStatusBar;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
